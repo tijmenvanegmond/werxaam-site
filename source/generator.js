930a7439fda0ee68baf5c6b193500a3894351abc
TvE <tym_time@hotmail.com>
2019-02-02 12:01:43 +0100

    Init

---
+++ source/generator.js
@@ -0,0 +1,29 @@
+const pug = require('pug');
+//filesystem
+const fs = require('fs');
+const inputPath = "./pages/";
+const outputPath = "../output/";
+
+
+fs.readdir(inputPath, (err, files) => {
+    files.forEach(fileName => {
+        if (fileName.includes(".pug")) {
+            let name = fileName.replace(".pug","")
+            renderPage(name);
+        }
+    });
+})
+
+
+function renderPage(pageName) {
+    let htmlRender = pug.renderFile(`${inputPath}${pageName}.pug`);
+
+    fs.writeFile(`${outputPath}${pageName}.html`, htmlRender, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(`The file was saved @${outputPath}${pageName}.html`);
+
+    })
+};
