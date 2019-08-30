fea984d1cd754c70ab8b9886f43855ede72c5b3d
Tijmen <tijmenvanegmond@mdmail.nl>
2019-08-30 11:01:06 +0200

    moved output to docs

--- source/generator.js
+++ source/generator.js
@@ -1,30 +1,28 @@
 const pug = require('pug');
 const fs = require('fs');
 const setupData = JSON.parse(fs.readFileSync('setup-data.json', 'utf8'));
-const inputPath = setupData.inputPath || "pages/";
-const outputPath = setupData.outputPath || "../output/";
+const inputPath = setupData.inputPath || 'pages/';
+const outputPath = setupData.outputPath || '../output/';
 
 fs.readdir(inputPath, (err, files) => {
-    if(files === undefined)    
-        throw(`Could not find any files in${inputPath}`);
+    if (files === undefined) throw `Could not find any files in${inputPath}`;
 
     files.forEach(fileName => {
-        if (fileName.includes(".pug")) {
-            let name = fileName.replace(".pug","")
+        if (fileName.includes('.pug')) {
+            let name = fileName.replace('.pug', '');
             renderPage(name);
         }
     });
-})
+});
 
 function renderPage(pageName) {
     let htmlRender = pug.renderFile(`${inputPath}${pageName}.pug`);
 
-    fs.writeFile(`${outputPath}${pageName}.html`, htmlRender, function (err) {
+    fs.writeFile(`${outputPath}${pageName}.html`, htmlRender, function(err) {
         if (err) {
             return console.log(err);
         }
 
         console.log(`A file was saved in ${outputPath}${pageName}.html`);
-
-    })
-};
+    });
+}
